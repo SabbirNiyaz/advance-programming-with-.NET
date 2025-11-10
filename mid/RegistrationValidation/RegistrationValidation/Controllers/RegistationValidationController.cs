4c691b79dc09f2c93f6bcb2ed456683bb528dfc7
Sabbir Niayz <sabbirlm10niyaz@gmail.com>
2025-11-10 22:25:43 +0600

    Registation Form validation

---
+++ mid/RegistrationValidation/RegistrationValidation/Controllers/RegistationValidationController.cs
@@ -0,0 +1,30 @@
+﻿using RegistrationValidation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RegistrationValidation.Controllers
+{
+    public class RegistationValidationController : Controller
+    {
+        // GET: RegistationValidation
+        [HttpGet]
+        public ActionResult RegistationForm()
+        {
+            return View(new FormValidation());
+        }
+
+        [HttpPost]
+        public ActionResult RegistationForm(FormValidation fv)
+        {
+            if (ModelState.IsValid)
+            {
+                TempData["Msg"] = "Registation Successfull";
+                return RedirectToAction("RegistationForm");
+            }
+            return View(fv);
+        }
+    }
+}
---
+++ mid/RegistrationValidation/RegistrationValidation/Models/FormValidation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RegistrationValidation.Models
+{
+    public class FormValidation
+    {
+        [Required]
+        [Range(1,40)]
+        public int Id { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+
+        [Required]
+        public string Profession { get; set; }
+
+        [Required]
+        public string[] Hobbies { get; set; }
+    }
+}
