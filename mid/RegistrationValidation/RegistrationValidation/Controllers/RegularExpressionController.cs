87da84eb3bac852bf6fe7dda681979cf24cedb90
Sabbir Niayz <sabbirlm10niyaz@gmail.com>
2025-11-11 01:43:57 +0600

    add Regex validation

---
+++ mid/RegistrationValidation/RegistrationValidation/Controllers/RegularExpressionController.cs
@@ -0,0 +1,31 @@
+﻿using RegistrationValidation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RegistrationValidation.Controllers
+{
+    public class RegularExpressionController : Controller
+    {
+        // GET: RegularExpression
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(new Models.Regex());
+        }
+
+        [HttpPost]
+        public ActionResult Index(Models.Regex regex)
+        {
+            if (ModelState.IsValid)
+            {
+               TempData["Msg"] = "Registration successful!";
+               return RedirectToAction("Index");
+            }
+            return View(regex);
+        }
+    }
+}
---
+++ mid/RegistrationValidation/RegistrationValidation/Models/Regex.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RegistrationValidation.Models
+{
+    public class Regex
+    {
+        [Required]
+        [RegularExpression("^[a-zA-Z ]+$", ErrorMessage = "Only letters and spaces allowed.")]
+        public string Name { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", ErrorMessage = "Invalid email format.")]
+        public string Email { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(?:\+88|88)?01[3-9]\d{8}$", ErrorMessage = "Invalid Bangladeshi mobile number.")]
+        public string Phone { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$",
+            ErrorMessage = "Password must have uppercase, lowercase, number, and 8+ chars.")]
+        public string Password { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\d{4}$", ErrorMessage = "Postal code must be 4 digits.")]
+        public string PostalCode { get; set; }
+    }
+}
